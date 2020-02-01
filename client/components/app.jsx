d8a839037851efae2289dc7ee241807acc88a1ca
Vicky Huynhcao <vicky89@gmail.com>
2020-02-01 08:25:30 +0000

    Added a banner image

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './header';
+import Banner from './banner';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header/>
+        <Banner/>
       </>
     );
   }
---
+++ client/components/banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="banner"></div>
+      </div>
+    );
+  }
+}
+
+export default Banner;
