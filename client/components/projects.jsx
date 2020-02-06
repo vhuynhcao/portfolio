97107e10ef544e6972f8d5cec19adde51fd7cbad
Vicky Huynhcao <vicky89@gmail.com>
2020-02-06 02:08:35 +0000

    Styled each projects into button

--- client/components/projects.jsx
+++ client/components/projects.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 class Projects extends React.Component {
   render() {
-    return null;
+    return (
+      <>
+        <h1 className="text-center mb-5">Projects</h1>
+        <div className="container d-flex justify-content-between mb-5">
+          <div type="button" className="btn btn-light">
+          One Piece Memory Match
+          </div>
+          <div type="button" className="btn btn-light">
+          Simply Creative
+          </div>
+          <div type="button" className="btn btn-light">
+          Spontaneous.ly App
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
