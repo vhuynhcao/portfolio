15bf37a31a372e0ea1da826a8d16cf08d2e69ba1
Vicky Huynhcao <vicky89@gmail.com>
2020-02-11 21:33:54 +0000

    Fixed some sizing with text and flexbox on three sections

--- client/components/skills.jsx
+++ client/components/skills.jsx
@@ -7,19 +7,15 @@ class Skills extends React.Component {
         <div className="text-center mb-5 pt-5 font-vellesa">
           Technical Skills
         </div>
-        <div className="container container-md dev-icon mb-5">
-          <div className="row d-flex justify-content-between mb-5">
-            <i className="devicon-html5-plain-wordmark"></i>
-            <i className="devicon-css3-plain-wordmark"></i>
-            <i className="devicon-javascript-plain"></i>
-            <i className="devicon-jquery-plain-wordmark"></i>
-          </div>
-          <div className="row d-flex justify-content-between col-md-12">
-            <i className="devicon-react-original-wordmark"></i>
-            <i className="devicon-php-plain"></i>
-            <i className="devicon-mysql-plain-wordmark"></i>
-            <i className="devicon-bootstrap-plain-wordmark"></i>
-          </div>
+        <div className="container d-flex flex-wrap justify-content-between dev-icon mb-5">
+          <i className="devicon-html5-plain-wordmark "></i>
+          <i className="devicon-css3-plain-wordmark"></i>
+          <i className="devicon-javascript-plain"></i>
+          <i className="devicon-jquery-plain-wordmark"></i>
+          <i className="devicon-react-original-wordmark"></i>
+          <i className="devicon-php-plain"></i>
+          <i className="devicon-mysql-plain-wordmark"></i>
+          <i className="devicon-bootstrap-plain-wordmark"></i>
         </div>
       </div>
     );
--- client/components/tools.jsx
+++ client/components/tools.jsx
@@ -5,25 +5,19 @@ class Tools extends React.Component {
     return (
       <div className="bg-light text-dark tools">
         <div className="text-center mb-5 pt-5 font-vellesa">Tools</div>
-        <div className="container container-md mb-5 dev-icon">
-          <div className="row d-flex justify-content-between mb-5">
-            <i className="devicon-github-plain-wordmark"></i>
-            <i className="devicon-photoshop-line"></i>
-            <i className="devicon-amazonwebservices-plain-wordmark"></i>
-            <i className="fab fa-figma"></i>
-          </div>
-          <div className="row d-flex justify-content-between mb-5">
-            <i className="devicon-chrome-plain"></i>
-            <i className="devicon-babel-plain"></i>
-            <i className="devicon-visualstudio-plain"></i>
-            <i className="fab fa-npm"></i>
-          </div>
-          <div className="row d-flex justify-content-between mb-5">
-            <i className="devicon-docker-plain-wordmark"></i>
-            <i className="devicon-slack-plain"></i>
-            <i className="devicon-ubuntu-plain"></i>
-            <i className="devicon-webpack-plain"></i>
-          </div>
+        <div className="container d-flex flex-wrap justify-content-between mb-5 dev-icon">
+          <i className="devicon-github-plain-wordmark"></i>
+          <i className="devicon-photoshop-line"></i>
+          <i className="devicon-amazonwebservices-plain-wordmark"></i>
+          <i className="fab fa-figma"></i>
+          <i className="devicon-chrome-plain"></i>
+          <i className="devicon-babel-plain"></i>
+          <i className="devicon-visualstudio-plain"></i>
+          <i className="fab fa-npm"></i>
+          <i className="devicon-docker-plain-wordmark"></i>
+          <i className="devicon-slack-plain"></i>
+          <i className="devicon-ubuntu-plain"></i>
+          <i className="devicon-webpack-plain"></i>
         </div>
       </div>
     );
