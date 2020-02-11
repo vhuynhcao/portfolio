50ac841d3c8374a7a04bb72db17c60f13186bf3e
Vicky Huynhcao <vicky89@gmail.com>
2020-02-11 23:41:08 +0000

    Made buttons on header work to direct to the correct section

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 class Header extends React.Component {
   render() {
@@ -9,12 +10,60 @@ class Header extends React.Component {
             VH
           </div>
           <div className="d-flex float-right justify-content-between">
-            <i className="fas fa-circle mr-2"></i>
-            <i className="fas fa-circle mr-2"> </i>
-            <i className="fas fa-circle mr-2"> </i>
-            <i className="fas fa-circle mr-2"> </i>
-            <i className="fas fa-circle mr-2"> </i>
-            <i className="fas fa-circle"></i>
+            <Link
+              to="banner"
+              smooth={true}
+              spy={true}
+              activeClass="active"
+              offset={-70}
+              duration={500}>
+              <i className="fas fa-circle mr-2"></i>
+            </Link>
+            <Link
+              to="about-me"
+              smooth={true}
+              spy={true}
+              activeClass="active"
+              offset={-70}
+              duration={500}>
+              <i className="fas fa-circle mr-2"> </i>
+            </Link>
+            <Link
+              to="projects"
+              smooth={true}
+              spy={true}
+              activeClass="active"
+              offset={-70}
+              duration={500}>
+              <i className="fas fa-circle mr-2"> </i>
+            </Link>
+            <Link
+              to="skills"
+              smooth={true}
+              spy={true}
+              activeClass="active"
+              offset={-70}
+              duration={500}>
+              <i className="fas fa-circle mr-2"> </i>
+            </Link>
+            <Link
+              to="tools"
+              smooth={true}
+              spy={true}
+              activeClass="active"
+              offset={-70}
+              duration={500}>
+              <i className="fas fa-circle mr-2"> </i>
+            </Link>
+            <Link
+              to="contact"
+              smooth={true}
+              spy={true}
+              activeClass="active"
+              offset={-70}
+              duration={500}>
+              <i className="fas fa-circle"></i>
+            </Link>
           </div>
         </nav>
       </>
